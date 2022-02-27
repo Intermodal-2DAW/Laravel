d048bbe9019835416020f2e065d9871683f14633
L3ORIOS <83226029+L3ORIOS@users.noreply.github.com>
2022-02-27 18:31:40 +0100

    Leo: Cambio de autorizacion en usuario

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -11,7 +11,7 @@ class UserController extends Controller
 
     public function __construct()
     {
-        $this->middleware('auth:api', ['except' => ['index', 'show']]);
+        $this->middleware('auth:api', ['except' => ['index', 'show','store','update']]);
     }
     /**
      * Display a listing of the resource.
